1114d1781fd57a59454ce28d38c21938c4d02abe
IanFlores07 <ian.flores07@somo.eus>
2026-01-13 13:27:45 +0100

    Atributos añadidos a la clase "Libros"

--- BibliotecaMuskiz/src/Classes/Libros.java
+++ BibliotecaMuskiz/src/Classes/Libros.java
@@ -1,4 +1,18 @@
 package Classes;
 public class Libros {
+    private int id_titulo;
+    private int existencias;
+    private int numero_paginas;
     private String titulo;
+    private String autor;
+    private String genero; //Se refiere a si es Literatura dramática, lírica, narrativa...
+    private String anio_publicacion;
+    private String editorial;
+    private String isbn;
+    private String idioma;
+    private String formato;
+    private String portada; //Se mete un link de la portada
+    private String descripcion;
+    private String categoria; //Se refiere a si es de Literatura, Comedia, Drama...
+    private boolean disponibilidad;
 }
