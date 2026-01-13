55c14378a61b4b4f25c53af9d9856239bd1e0dc6
IanFlores07 <ian.flores07@somo.eus>
2026-01-13 13:31:04 +0100

    Definición de atributos de la clase "Libros"

--- BibliotecaMuskiz/src/Classes/Libros.java
+++ BibliotecaMuskiz/src/Classes/Libros.java
@@ -15,4 +15,21 @@ public class Libros {
     private String descripcion;
     private String categoria; //Se refiere a si es de Literatura, Comedia, Drama...
     private boolean disponibilidad;
+
+     public Libros(int id_titulo, int existencias, int numero_paginas, String titulo, String autor, String genero, String anio_publicacion, String editorial, String isbn, String idioma, String formato, String portada, String descripcion, String categoria, boolean disponibilidad) {
+        this.id_titulo = id_titulo;
+        this.existencias = existencias;
+        this.numero_paginas = numero_paginas;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+        this.anio_publicacion = anio_publicacion;
+        this.editorial = editorial;
+        this.isbn = isbn;
+        this.idioma = idioma;
+        this.formato = formato;
+        this.portada = portada;
+        this.descripcion = descripcion;
+        this.categoria = categoria;
+        this.disponibilidad = disponibilidad;
 }
