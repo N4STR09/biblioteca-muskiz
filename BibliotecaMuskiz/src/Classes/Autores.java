be2b166ffa8ef03c7de790c84ac20107a29ba991
nstr <erpro.ferru@gmail.com>
2026-01-13 13:31:14 +0100

    Definición de atributos de la clase "Autores"

--- BibliotecaMuskiz/src/Classes/Autores.java
+++ BibliotecaMuskiz/src/Classes/Autores.java
@@ -1,4 +1,30 @@
 package Classes;
 public class Autores {
+    private int idAutor;
+    private String nombre;
+    private String nacionalidad;
+    private String fechaNacimiento;
+    private boolean defuncion;
+    private String fechaFallecimiento;
+    private String biografia;
+    private String foto;
+    private String generoLiterario;
+    private String premios;
+    private String obrasDestacadas;
 
+    public Autores(int idAutor, String nombre, String nacionalidad, String fechaNacimiento, boolean defuncion, String fechaFallecimiento, String biografia, String foto, String generoLiterario, String premios, String obrasDestacadas) {
+        this.idAutor = idAutor;
+        this.nombre = nombre;
+        this.nacionalidad = nacionalidad;
+        this.fechaNacimiento = fechaNacimiento;
+        this.defuncion = defuncion;
+        this.fechaFallecimiento = fechaFallecimiento;
+        this.biografia = biografia;
+        this.foto = foto;
+        this.generoLiterario = generoLiterario;
+        this.premios = premios;
+        this.obrasDestacadas = obrasDestacadas;
+    }
+
+    
 }
