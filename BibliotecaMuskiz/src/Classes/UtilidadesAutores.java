f652d79ee156ba90b5e74d9459a854942ddc34f9
IanFlores07 <ian.flores07@somo.eus>
2026-01-22 10:37:15 +0100

    Ideas de utilidadesautores

--- BibliotecaMuskiz/src/Classes/UtilidadesAutores.java
+++ BibliotecaMuskiz/src/Classes/UtilidadesAutores.java
@@ -3,5 +3,26 @@ package Classes;
 import java.util.List;
 
 public class UtilidadesAutores {
+    /*
+    Bloque 1: Estadisticas de libros por autor
+    -----------------------------
+    Autor que más libros tiene en la biblioteca
+    Autor que menos libros tiene en la biblioteca
     
+    Bloque 3: Estadisticas de paginas por libro de autor
+    -----------------------------
+    Autor con el libro con más largo
+    Autor con el libro con más corto
+    
+    Bloque 4: Edades de autores
+    -----------------------------
+    Autor más viejo
+    Autor más joven
+    Edad media de los autores
+    
+    Bloque 4: Estadisticas de autores
+    -----------------------------
+    Total de Autores
+    */
+   
 }
