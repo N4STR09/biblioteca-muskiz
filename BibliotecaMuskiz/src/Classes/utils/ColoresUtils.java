7323c1be5a54be4a2dff484ca0e0af3ca636a6a4
nstr <erpro.ferru@gmail.com>
2026-02-03 13:26:48 +0100

    Definicion de colores para usar

---
+++ BibliotecaMuskiz/src/Classes/utils/ColoresUtils.java
@@ -0,0 +1,34 @@
+package Classes.utils;
+
+public class ColoresUtils {
+    // Reset
+    public static final String RESET = "\u001B[0m";
+
+    // Colores de texto
+    public static final String NEGRO  = "\u001B[30m";
+    public static final String ROJO   = "\u001B[31m";
+    public static final String VERDE  = "\u001B[32m";
+    public static final String AMARILLO = "\u001B[33m";
+    public static final String AZUL   = "\u001B[34m";
+    public static final String MORADO = "\u001B[35m";
+    public static final String CYAN   = "\u001B[36m";
+    public static final String BLANCO = "\u001B[37m";
+
+    // Colores de fondo
+    public static final String BG_NEGRO  = "\u001B[40m";
+    public static final String BG_ROJO   = "\u001B[41m";
+    public static final String BG_VERDE  = "\u001B[42m";
+    public static final String BG_AMARILLO = "\u001B[43m";
+    public static final String BG_AZUL   = "\u001B[44m";
+    public static final String BG_MORADO = "\u001B[45m";
+    public static final String BG_CYAN   = "\u001B[46m";
+    public static final String BG_BLANCO = "\u001B[47m";
+
+    // Estilos de texto
+    public static final String NEGRITA     = "\u001B[1m";
+    public static final String SUBRAYADO   = "\u001B[4m";
+    public static final String PARPADEO    = "\u001B[5m";
+    public static final String INVERSO     = "\u001B[7m";
+    public static final String OCULTO      = "\u001B[8m";
+}
+
