fdde464c7daf2670f253a175821318dc5dae6c9f
nstr <erpro.ferru@gmail.com>
2026-02-12 13:22:46 +0100

    Correccion nombre clases v2

--- BibliotecaMuskiz/src/Classes/menu/MenuLibros.java
+++ BibliotecaMuskiz/src/Classes/menu/MenuLibros.java
@@ -3,16 +3,29 @@ package Classes.menu;
 import java.util.List;
 import java.util.Scanner;
 
-import Classes.model.Autores;
-import Classes.model.Libros;
+import Classes.model.*;
 import Classes.service.ServiceLibros;
 import Classes.utils.ColoresUtils;
 import Classes.utils.InputUtils;
 import Classes.utils.TitlesUtils;
 
 public class MenuLibros {
-    public static void mostrar(Scanner sc, List<Autores> autores, List<Libros> libros) {
+    public static void mostrar(Scanner sc, List<Autor> autores, List<Libro> libros) {
+        
+        System.out.println("1. " + ColoresUtils.VERDE + "Añadir " + ColoresUtils.RESET + "libro");
+        System.out.println("2. " + ColoresUtils.ROJO + "Eliminar " + ColoresUtils.RESET + "libro");
+        System.out.println("3. " + ColoresUtils.AZUL + "Visualizar " + ColoresUtils.RESET + "libro");
+        System.out.println("4. " + ColoresUtils.AMARILLO + "Estadísticas" + ColoresUtils.RESET);
+        
         TitlesUtils.tituloLibros();
+        System.out.println("\n1. " + ColoresUtils.VERDE_OSCURO + "Añadir " + ColoresUtils.RESET + "libro");
+        System.out.println("2. " + ColoresUtils.ROJO_OSCURO + "Eliminar " + ColoresUtils.RESET + "libro");
+        System.out.println("3. " + ColoresUtils.AZUL_INTENSO + "Visualizar " + ColoresUtils.RESET + "libro");
+        System.out.println("4. " + ColoresUtils.AMARILLO_MOSTAZA + "Estadisticas" + ColoresUtils.RESET);
+        System.out.println("\n1. " + ColoresUtils.VERDE_OSCURO + "Añadir " + ColoresUtils.RESET + "libro");
+        System.out.println("2. " + ColoresUtils.ROJO_OSCURO + "Eliminar " + ColoresUtils.RESET + "libro");
+        System.out.println("3. " + ColoresUtils.AZUL_INTENSO + "Visualizar " + ColoresUtils.RESET + "libro");
+        System.out.println("4. " + ColoresUtils.AMARILLO_MOSTAZA + "Estadisticas" + ColoresUtils.RESET);
         System.out.println("1. " + ColoresUtils.VERDE + "Añadir " + ColoresUtils.RESET + "libro");
         System.out.println("2. " + ColoresUtils.ROJO + "Eliminar " + ColoresUtils.RESET + "libro");
         System.out.println("3. " + ColoresUtils.AZUL + "Visualizar " + ColoresUtils.RESET + "libro");
